33663778a4e3b9aaf11ecfd2fb5dcab6ad7d9ff0
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2019-01-05 23:46:01 +0300

    Add functionality for show list users

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -14,31 +14,42 @@
     <hr style="border: 1px solid #333;">
     <Channels/>
     <hr style="border: 1px solid #333;">
+    <Users/>
+    <hr style="border: 1px solid #333;">
     <button class="btn btn-outline-light" @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
 import auth from "firebase/auth";
+import database from "firebase/database";
 import { mapActions, mapGetters } from "vuex";
 
 import Channels from "./Channels";
+import Users from "./Users";
 
 export default {
   name: "sidebar",
+  data() {
+    return {
+      presenceRef: firebase.database().ref("presence")
+    };
+  },
   computed: {
     ...mapGetters(["currentUser"])
   },
   methods: {
     ...mapActions(["setUser"]),
     async logout() {
+      this.presenceRef.child(this.currentUser.uid).remove();
       await firebase.auth().signOut();
       await this.setUser(null);
       this.$router.push("/login");
     }
   },
   components: {
-    Channels
+    Channels,
+    Users
   }
 };
 </script>
